9a77502a1f15fcd67cc50e7e5bf766d8f7c4e57c
DarmaPutra55 <igustiagunggededarmaputra28@gmail.com>
2022-08-19 21:37:00 +0800

    Use FakeStore api's cart to make a fake cart item.

--- src/components/context/chartContext.js
+++ src/components/context/chartContext.js
@@ -1,26 +1,29 @@
+import { Flex, useBoolean } from "@chakra-ui/react";
 import { createContext, useEffect, useState } from "react";
 import fetchApi from "../smallcomponent/fetchApi/fetchApi";
+import Loading from "../smallcomponent/loading/loading";
 
 export const ChartContext = createContext();
 
 export default function ChartContextProvider({ children }) {
     const [items, setItems] = useState([]);
     const [totalChartPrice, setTotalChartPrice] = useState(0);
+    const [isLoading, setIsLoading] = useBoolean(true);
 
 
-    const removeItem = (chartID)=>{
-        setItems(()=>{
-            return items.filter((element)=> element.chartID !== chartID)
+    const removeItem = (chartID) => {
+        setItems(() => {
+            return items.filter((item) => item.chartID !== chartID)
         })
     }
 
-    const addItem = (item)=> {
+    const addItem = (item) => {
         setItems([...items, item])
     }
 
-    const checkItem = (itemID) =>{
-        items.forEach(element => {
-            if(element.itemID === itemID){
+    const checkItem = (itemID) => {
+        items.forEach(item => {
+            if (item.itemID === itemID) {
                 return true
             }
         })
@@ -29,40 +32,86 @@ export default function ChartContextProvider({ children }) {
     }
 
     const itemCheckHandler = (chartID, checked) => {
-        setItems(()=>{
-            return items.map((element)=>{
-                if(element.chartID === chartID){
-                    element.checked = checked;
+        setItems(() => {
+            return items.map((item) => {
+                if (item.chartID === chartID) {
+                    item.checked = checked;
                 }
 
-                return element
+                return item
             })
         })
     }
 
-    const changeChartItemQuantity = (chartID, itemQuantity) =>{
-        setItems(()=>{
-            return items.map((element)=>{
-                if(element.chartID === chartID){
-                    element.itemQuantity = itemQuantity
+    const changeChartItemQuantity = (chartID, itemQuantity) => {
+        setItems(() => {
+            console.log(items)
+            return items.map((item) => {
+                if (item.chartID === chartID) {
+                    item.itemQuantity = itemQuantity
                 }
 
-                return element;
+                return item;
             })
         })
     }
 
-    useEffect(()=>{
-       //initializeItems();
+    const initializeItems = async () => {
+        try {
+            setIsLoading.on();
+            const fetchedItems = await fetchApi("https://fakestoreapi.com/carts/1");
+            console.log(fetchedItems.products);
+            const temp_items = await Promise.all(fetchedItems.products.map(async (fetchedItem, index) => {
+                const fetchedItemDetail = await fetchApi("https://fakestoreapi.com/products/" + fetchedItem.productId);
+                const item = {
+                    "chartID": index,
+                    "itemID": fetchedItem.productId,
+                    "itemImg": fetchedItemDetail.image,
+                    "itemName": fetchedItemDetail.title,
+                    "itemQuantity": fetchedItem.quantity,
+                    "itemPrice": fetchedItemDetail.price,
+                    "itemStock": 20,
+                    "checked": true,
+                };
+
+                return item;
+            }))
+
+            setItems(temp_items)
+        }
+
+        catch (err) {
+            console.log(err);
+        }
+
+        finally {
+            setIsLoading.off();
+        }
+    }
+
+    useEffect(() => {
+        initializeItems();
     }, [])
 
-    useEffect(()=>{
-        setTotalChartPrice(items.length > 0 ? items.reduce((previousPrice, item)=> item.checked ? previousPrice+item.itemPrice * item.itemQuantity : previousPrice , 0) : 0)
+    useEffect(() => {
+        setTotalChartPrice(items.length > 0 ? items.reduce((previousPrice, item) => item.checked ? previousPrice + item.itemPrice * item.itemQuantity : previousPrice, 0) : 0)
     }, [items])
 
-    return(
-        <ChartContext.Provider value={{items, changeChartItemQuantity, itemCheckHandler, addItem, removeItem, totalChartPrice}}>
-            { children }
+    return (
+        <ChartContext.Provider value={{ items, changeChartItemQuantity, itemCheckHandler, addItem, removeItem, totalChartPrice }}>
+            {
+                isLoading ? 
+                    <Flex
+                        minH={"100vh"}
+                        minW={"100vw"}
+                        alignItems={"center"}
+                        justifyContent={"center"}
+                    >
+                        <Loading />
+                    </Flex> 
+                : 
+                    children
+            }
         </ChartContext.Provider>
     )
 }
