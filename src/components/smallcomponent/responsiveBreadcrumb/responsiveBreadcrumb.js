e1ef9e7ffb5ab9e77d9e80639c0dbb448532bcbc
DarmaPutra55 <igustiagunggededarmaputra28@gmail.com>
2022-10-24 11:50:52 +0800

    Added padding to the breadcrumb.

--- src/components/smallcomponent/responsiveBreadcrumb/responsiveBreadcrumb.js
+++ src/components/smallcomponent/responsiveBreadcrumb/responsiveBreadcrumb.js
@@ -2,7 +2,7 @@ import { Breadcrumb, Flex } from "@chakra-ui/react";
 
 export default function ResponsiveBreadcrumb({children}){
     return(
-        <Flex className={"responsiveWidth"}>
+        <Flex className={"responsiveWidth"} px={"10px"}>
             <Breadcrumb fontWeight={"bold"} fontSize={"xl"} alignSelf={"flex-start"}>
                 {children}
             </Breadcrumb>
